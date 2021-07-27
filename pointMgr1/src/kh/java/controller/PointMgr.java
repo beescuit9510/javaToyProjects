c7b76b49a7545ef49095c43e9ba2a87c5d7f8b30
yunyoung jang <pengsoon20@gmail.com>
2021-07-27 10:25:50 +0900

    update pointMgr

--- pointMgr1/src/kh/java/controller/PointMgr.java
+++ pointMgr1/src/kh/java/controller/PointMgr.java
@@ -2,24 +2,32 @@ package kh.java.controller;
 
 import java.util.Scanner;
 
+import kh.java.vo.Gold;
 import kh.java.vo.Silver;
+import kh.java.vo.Vip;
 
 public class PointMgr {
-	Silver[] silvers;
-	Scanner sc;
-	int index;
+	private Vip[] v;
+	private Gold[] g;
+	private Silver[] s;
+	private Scanner sc;
+	private int vIndex;
+	private int gIndex;
+	private int sIndex;
 	
 
 	
 	public PointMgr() {
-		silvers = new Silver[10];
+		v = new Vip[10];
+		g = new Gold[10];
+		s = new Silver[10];
 		sc = new Scanner(System.in);
-		index = 0;
+		sIndex = 0;
 	}
 	public void main() {
 
 		while(true) {			
-			System.out.println("===== 포인트 관리 프로그램v1 =====");
+			System.out.println("\n============= 포인트 관리 프로그램v1 ===========\n");
 			System.out.println("1. 회원 정보 등록");
 			System.out.println("2. 전체 회원 조회");
 			System.out.println("3. 회원 1명 조회");
@@ -61,25 +69,27 @@ public class PointMgr {
 	
 	
 	public void insertMember   (){
-		System.out.println("=============== 회원 정보 입력 ==============");
+		System.out.println("\n=============== 회원 정보 입력 ==============\n");
 		System.out.printf("등급/이름/포인트 입력 (띄어쓰기로 구분): ");
-		silvers[index] = new Silver(sc.next(), sc.next(), sc.next());
+		s[sIndex++] = new Silver(sc.next(), sc.next(), sc.nextInt());
 		System.out.println("등록 완료!");
 	}
 	public void printAllMember (){
-		System.out.println("=============== 전체 회원 정보 출력 ==============");
-		for(int i=0;i<index;i++) {
-			System.out.printf("%s ';[[[[[[[/....d%s %s", silvers[i].getGrade(), silvers[i].getName(), silvers[i].getPoint());
+		System.out.println("\n=============== 전체 회원 정보 출력 ==============\n");
+		System.out.println("등급\t이름\t포인트\t보너스");
+		for(int i=0;i<sIndex;i++) {
+			System.out.printf("%s\t%s\t%d\t%.4f%n",s[i].getGrade(), s[i].getName(), s[i].getPoint(), s[i].getBonus());
 		}
 	}
-	public void printOneMember (){}
+	public void printOneMember (){
+	}
 	public void updateMember   (){}
 	public void deleteMember   (){}
 	
 	
 	public int searchIndex(String name){
-		for(int i=0;i<index;i++) {
-			if(silvers[i].getName().equals(name)) {
+		for(int i=0;i<sIndex;i++) {
+			if(s[i].getName().equals(name)) {
 				return i;
 			}
 		}
--- pointMgr1/src/kh/java/vo/Vip.java
+++ pointMgr1/src/kh/java/vo/Vip.java
@@ -1,42 +1,50 @@
 package kh.java.vo;
 
-public class Silver {
+public class Vip {
 	
 	private String grade;
 	private String name;
 	private int point;
 	
 	
-	public Silver() {
+	public Vip() {
 		
 	}
 	
-	public Silver(String grade, String name, int point) {
+	public Vip(String grade, String name, int point) {
 		this.grade = grade;
 		this.name = name;
 		this.point = point;
 	}
+	
 	public String getGrade() {
 		return grade;
 	}
+	
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getPoint() {
 		return point;
 	}
+	
 	public void setPoint(int point) {
 		this.point = point;
 	}
 	
 	public double getBonus() {
-		return 0.02*this.point;
+		return 0.07*this.point;
 	}
+	
+
 	
 }
