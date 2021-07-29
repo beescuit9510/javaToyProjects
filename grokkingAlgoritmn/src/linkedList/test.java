5cb81153c7dc8d15ee2f41763bf931db7820d710
yunyoung jang <pengsoon20@gmail.com>
2021-07-29 12:58:41 +0900

    update LinkedList

---
+++ grokkingAlgoritmn/src/linkedList/test.java
@@ -0,0 +1,28 @@
+package linkedList;
+import java.util.LinkedList;
+import linkedList.*;
+public class test {
+
+	public static void main(String[] args) {
+		
+		Methods m = new Methods();
+//		MyLinkedList m = MyLinkedList();
+		
+//		m.signUp()
+		m.signUP();
+		m.printMember();
+		m.printMember();
+	}
+
+	
+//	@Test
+//	public void test(){
+//		MyLinkedList list = new MyLinkedList();
+//		list.add(new Node(15));
+//		list.add(new Node(16));
+//		list.add(new Node(10));
+//		
+//		System.out.println("done");
+//	}
+
+}
